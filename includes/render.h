0f7a5f1637b94528b472ba07fed856fdce9ef86c
Jean-Jacques MOIROUX <jmoiroux@student.42.fr>
2015-02-09 12:13:35 +0100

    ajout render.h pour ncurses

---
+++ includes/render.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoiroux <jmoiroux@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/09 11:47:09 by jmoiroux          #+#    #+#             */
+/*   Updated: 2015/02/09 11:47:11 by jmoiroux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RENDER_H
+# define RENDER_H
+
+# define WINX 254		// default 254
+# define WINY 68		// default 68
+
+// L = Legends
+# define L_X_START 197	// default 197
+# define L_Y_START 0 	// default 0
+# define L_Y_STATUS 2 	// default 2
+# define L_Y_SEC_CLE 4	// default 4 "Cycles/second limit :"
+# define L_Y_CYCLE 7	// default 7 "Cycle :"
+# define L_Y_PROCES 9	// default 10 "Processes :"
+
+# define L_Y_PLAY_1 11	// default 11 "Player 1"
+
+# define L_Y_PLAY_2 15	// default 15 "Player 2"
+
+# define L_Y_PLAY_3 19	// default 19 "Player 2"
+
+# define L_Y_PLAY_2 23	// default 23 "Player 2"
+
+#endif
