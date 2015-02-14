4dac5041b58720a76de641b84208ee47a0e5934b
Vincent Jacquier <vin.jacquier@gmail.com>
2015-02-14 18:02:22 +0100

    ajout corewarOpTab.h

--- corewar_src/checkNextOp.c
+++ corewar_src/checkNextOp.c
@@ -6,11 +6,11 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/13 17:13:56 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/02/13 17:21:26 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/02/14 17:35:02 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/corewar.h"
+#include "../includes/corewarOpTab.h"
 
 int		checkNextOp(t_data *d)
 {
