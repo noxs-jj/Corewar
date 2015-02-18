f57a843fd17c5e1e94a997d5a148e83268c7e1fd
Vincent Jacquier <vin.jacquier@gmail.com>
2015-02-18 12:51:04 +0100

    op_live done (not tested)

--- corewar_src/op/op_live.c
+++ corewar_src/op/op_live.c
@@ -6,7 +6,7 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/13 17:27:32 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/02/17 15:03:40 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/02/18 12:50:36 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ int		op_live(t_data *d, t_header *player, int id)
 	t_case *arg;
 	int ret;
 	
-	if ((ret = getOpArgs(d, id)) < 0)
-		return (ret);
-
-	if (player->PC > 0 && player->PC < d->players)
-	{
-		player->wait = op_tab[player->nextOp][4];
-		d->prog[ft_hex2Dec(player->opArgs[0])].lastLive = d->cycle;
-		d->prog[player->PC].liveNbr++;
-	}
+	if ((ret = getOpArgs(d, id)) < 0 || ft_hex2Dec(player->opArgs[0]) < 0
+		|| ft_hex2Dec(player->opArgs[0]) > d->players)
+		return (-1);
+	player->wait = op_tab[player->nextOp][4];
+	d->prog[ft_hex2Dec(player->opArgs[0])].lastLive = d->cycle;
+	d->prog[player->PC].liveNbr++;
 	player->PC = ((player->PC) + ret);
 	return (0);
 }
