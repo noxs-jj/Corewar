c48ba9bf25fc10666d479d581f4461ef63e54106
fdeage <fdeage@gmail.com>
2015-02-23 13:32:32 +0100

    automatic commit: Mon Feb 23 13:32:32 CET 2015

--- fdeage/write.c
+++ fdeage/write.c
@@ -6,7 +6,7 @@
 /*   By: fdeage <fdeage@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/12 17:13:52 by fdeage            #+#    #+#             */
-/*   Updated: 2015/02/20 15:47:06 by fdeage           ###   ########.fr       */
+/*   Updated: 2015/02/23 13:24:35 by fdeage           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,22 @@ static void	write_name_comment(int fd, t_header header)
 static void	write_text_section(t_file *file)
 {
 	t_list	*tmp;
+	//virer
+	size_t	cuml=0;
 
 	tmp = file->lines;
 	fprintf(stderr, "WRITE:\n");
+
 	while (tmp)
 	{
+		cuml+=LINE->code_len;
 		if (LINE->bytecode)
 			write(file->fd_cor, LINE->bytecode, LINE->code_len);
-		fprintf(stderr, "len = %d line = : ", (int)LINE->code_len);
+		fprintf(stderr, "id: %d, str = \033[33m%s\033[0m, len = \033[31m%d\033[0m-\033[31m%d\033[0m line = |\033[1;34m", (int)LINE->id, LINE->str, (int)LINE->code_len, (int)cuml);
 		int i = 0;
 		while (i < (int)LINE->code_len)
 			fprintf(stderr, "%d ", (LINE->bytecode)[i++]);
-		fprintf(stderr, "\n");
+		fprintf(stderr, "\033[0m|\n");
 		tmp = tmp->next;
 	}
 }
