552bf1172b8b0c1ce79aea05e252bc8f0996239d
Vincent Jacquier <vin.jacquier@gmail.com>
2015-02-25 16:54:30 +0100

    add, aff, ld, sub done and tested OK

--- corewar_src/op/op_add.c
+++ corewar_src/op/op_add.c
@@ -6,14 +6,14 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/13 17:27:32 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/02/20 14:56:46 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/02/25 16:26:51 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/corewarOpTab.h"
 
 /*
-** not tested
+** tested OK
 ** T_REG, T_REG, T_REG
 */
 
@@ -24,6 +24,7 @@ int		op_add(t_data *d, t_header *player, int id)
 	unsigned int value;
 	char		str[9];
 	
+	writeL("--- op_add ---");
 	if ((ret = getOpArgs(d, id)) < 0
 		|| isValidRegister(ft_hex2Dec(player->opArgs[0])) < 0
 		|| isValidRegister(ft_hex2Dec(player->opArgs[1])) < 0
@@ -36,7 +37,7 @@ int		op_add(t_data *d, t_header *player, int id)
 	value += ft_hex2Dec(player->reg[ft_hex2Dec(player->opArgs[1])]);
 	ft_bzero(str, 9);
 	ft_putHexBNbr(value, str);
-	ft_strcpy(player->opArgs[reg], str);
+	ft_strcpy(player->reg[reg], str);
 	player->carry = true;
 	pcAdvance(d, player, ret);
 	return (0);
--- corewar_src/op/op_ld.c
+++ corewar_src/op/op_ld.c
@@ -6,14 +6,14 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/13 17:27:32 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/02/21 11:09:42 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/02/25 16:50:21 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/corewarOpTab.h"
 
 /*
-** not tested
+** tested OK
 ** T_DIR | T_IND, T_REG
 */
 
@@ -21,11 +21,14 @@ int		op_ld(t_data *d, t_header *player, int id)
 {
 	int ret;
 	unsigned int reg;
+	int value;
 	
+	writeL("--- op_ld ---");
 	if ((ret = getOpArgs(d, id)) < 0
 		|| isValidRegister(ft_hex2Dec(player->opArgs[1])) < 0)
 		return (ret);
-	reg = ft_hex2Dec(player->opArgs[2]);
+	reg = ft_hex2Dec(player->opArgs[1]);
+	value = ft_hex2intdec(player->opArgs[0]);
 	ft_bzero(player->opArgs[reg], REG_SIZE);
 	ft_strcpy(player->opArgs[reg], player->opArgs[0]);
 	player->carry = true;
