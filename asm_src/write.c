be1074785ff1c6094082063e7b3ebf167f456c03
fdeage <fdeage@gmail.com>
2015-02-27 11:28:02 +0100

    LAST: norme write.c

--- asm_src/write.c
+++ asm_src/write.c
@@ -6,7 +6,7 @@
 /*   By: fdeage <fdeage@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/12 17:13:52 by fdeage            #+#    #+#             */
-/*   Updated: 2015/02/23 20:59:36 by fdeage           ###   ########.fr       */
+/*   Updated: 2015/02/27 11:25:16 by fdeage           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,31 +41,18 @@ static void	write_name_comment(int fd, t_header header)
 static void	write_text_section(t_file *file)
 {
 	t_list	*tmp;
-	//virer
-	size_t	cuml=0;
 
 	tmp = file->lines;
-	fprintf(stderr, "WRITE:\n");
-
 	while (tmp)
 	{
-		cuml+=LINE->code_len; //virer
 		if (LINE->bytecode)
 			write(file->fd_cor, LINE->bytecode, LINE->code_len);
-		fprintf(stderr, "id: %d, str = \033[33m%s\033[0m, len = \033[31m%d\033[0m-\033[31m%d\033[0m line = |\033[1;34m", (int)LINE->id, LINE->str, (int)LINE->code_len, (int)cuml);
-
-		int i = 0;
-		while (i < (int)LINE->code_len)
-			fprintf(stderr, "%d ", (LINE->bytecode)[i++]);
-		fprintf(stderr, "\033[0m|\n");
-
 		tmp = tmp->next;
 	}
 }
 
 void		write_cor(t_file *file)
 {
-	fprintf(stderr, "file nb=%d\n", (int)file->nb_line);
 	if (file->nb_line == 0)
 		asm_error("No instruction in the file.\n");
 	if (!(file->fd_cor = open(file->name_cor, O_WRONLY | O_CREAT, 0644)))
