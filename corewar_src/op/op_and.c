8faeb03e3be6cceb2e3ad52f6687839d3d36fab1
Vincent Jacquier <vin.jacquier@gmail.com>
2015-02-27 13:54:17 +0100

    or, xor, and corrected

--- corewar_src/op/op_and.c
+++ corewar_src/op/op_and.c
@@ -6,7 +6,7 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/13 17:27:32 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/02/26 14:35:28 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/02/27 13:48:18 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,25 @@ int		op_and(t_data *d, t_header *player)
 	// writeL("codage data");
 	// writeL(d->prog[id].codage);
 	// sleep(5);
-
+	writeL("--- op_and ---");
 	if ((ret = getOpArgs(d, player)) < 0
 		|| isValidRegister(ft_hex2Dec(player->opArgs[2])) < 0)
 		return (ret);
 	reg = ft_hex2Dec(player->opArgs[2]);
 	if (ft_strncmp(player->codage, "01", 2) == 0
-		&& isValidRegister(ft_hex2Dec(player->opArgs[0])) < 0)
+		&& isValidRegister(ft_hex2Dec(player->opArgs[0])) >= 0)
 		value[0] = ft_hex2Dec(player->reg[ft_hex2Dec(player->opArgs[0])]);
 	else
 		value[0] = ft_hex2Dec(player->opArgs[0]);
 	if (ft_strncmp(&player->codage[2], "01", 2) == 0
-		&& isValidRegister(ft_hex2Dec(player->opArgs[1])) < 0)
+		&& isValidRegister(ft_hex2Dec(player->opArgs[1])) >= 0)
 		value[1] = ft_hex2Dec(player->reg[ft_hex2Dec(player->opArgs[1])]);
 	else
 		value[1] = ft_hex2Dec(player->opArgs[1]);
+	
 	ft_bzero(player->reg[reg], REG_SIZE);
 	ft_bzero(str, 9);
+
 	ft_putHexBNbr(value[0] & value[1], str);
 	ft_strcpy(player->reg[reg], str);
 	if (value[0] & value[1])
--- corewar_src/op/op_or.c
+++ corewar_src/op/op_or.c
@@ -6,7 +6,7 @@
 /*   By: vjacquie <vjacquie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/13 17:27:32 by vjacquie          #+#    #+#             */
-/*   Updated: 2015/02/26 14:36:23 by vjacquie         ###   ########.fr       */
+/*   Updated: 2015/02/27 13:49:19 by vjacquie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ int		op_or(t_data *d, t_header *player)
 		return (ret);
 	reg = ft_hex2Dec(player->opArgs[2]);
 	if (ft_strncmp(player->codage, "01", 2) == 0
-		&& isValidRegister(ft_hex2Dec(player->opArgs[0])) < 0)
+		&& isValidRegister(ft_hex2Dec(player->opArgs[0])) >= 0)
 		value[0] = ft_hex2Dec(player->reg[ft_hex2Dec(player->opArgs[0])]);
 	else
 		value[0] = ft_hex2Dec(player->opArgs[0]);
 	if (ft_strncmp(&player->codage[2], "01", 2) == 0
-		&& isValidRegister(ft_hex2Dec(player->opArgs[1])) < 0)
+		&& isValidRegister(ft_hex2Dec(player->opArgs[1])) >= 0)
 		value[1] = ft_hex2Dec(player->reg[ft_hex2Dec(player->opArgs[1])]);
 	else
 		value[1] = ft_hex2Dec(player->opArgs[1]);
