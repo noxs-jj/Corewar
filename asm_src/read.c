e597fdb42a9050b70a6ea73f801775c2f635765e
fdeage <fdeage@gmail.com>
2015-02-27 11:20:41 +0100

    LAST: norme read.c

--- asm_src/read.c
+++ asm_src/read.c
@@ -6,7 +6,7 @@
 /*   By: fdeage <fdeage@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/27 13:44:48 by fdeage            #+#    #+#             */
-/*   Updated: 2015/02/26 19:45:33 by fdeage           ###   ########.fr       */
+/*   Updated: 2015/02/27 11:19:58 by fdeage           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,27 @@
 #include "asm_fn.h"
 #include "libft.h"
 
+/*
+** - has_label() returns 0 if no ':' is found at the end of the token, else
+** returns the position of it
+** - if the line is of the type 'label: inst', add_line() splits the line
+** into two lines: 'label:' and 'inst'.
+** WARNING: if no inst follows the label, then only adds the label!
+*/
+
 static int	init_line(t_file *file, char *str, size_t i, int line_type)
 {
 	t_line	*line;
 	char	*final_comment;
 
-//	fprintf(stderr, "READINIT0=%s\n", str);
 	if (!(line = (t_line *)malloc(sizeof(t_line))))
 		return (EXIT_FAILURE);
 	line->tokens = NULL;
 	line->id = i;
 	line->code_len = 0;
-	//line->has_final_comment = has_final_comment(str);
 	if ((final_comment = ft_strchr(str, FINAL_COMMENT_CHAR)))
 	{
-		fprintf(stderr, "1: str = %s\n", str);
 		*final_comment = '\0';
-		fprintf(stderr, "2: str = %s\n", str);
-//		(line->str) = ;
 	}
 	line->str = ft_strtrim(str);
 	line->nb_params = 0;
@@ -40,15 +43,9 @@ static int	init_line(t_file *file, char *str, size_t i, int line_type)
 	ft_bzero(line->bytecode, 14);
 	line->pcode = -1;
 	ft_lstadd_back(&(file->lines), ft_lstnew((void *)line, sizeof(t_line)));
-	//fprintf(stderr, "READ INIT=%s\n", line->str);
 	free(line);
 	return (EXIT_SUCCESS);
 }
-
-/*
-** has_label() returns 0 if no ':' is found at the end of the token, else
-** returns the position of it
-*/
 
 static int	has_label(char *str)
 {
@@ -65,13 +62,6 @@ static int	has_label(char *str)
 	return (0);
 }
 
-/*
-** if the line is of the type 'label: inst', add_line() splits the line
-** into two lines: 'label:' and 'inst'.
-** WARNING: if no inst follows the label, then only adds the label!
-*/
-
-//OK - 21L
 static int	add_line(t_file *file, char *str, size_t *i, int has_label)
 {
 	size_t	len;
@@ -79,31 +69,23 @@ static int	add_line(t_file *file, char *str, size_t *i, int has_label)
 	len = ft_strlen(str);
 	if (has_label)
 	{
-		fprintf(stderr, "ADD LABEL: |%s| len=%d has_label=%d\n", str, (int)len, has_label);
 		str[has_label + 1] = 0;
-		fprintf(stderr, "has label READ INIT=%s\n", str);
 		if (init_line(file, str, *i, T_LABEL) == EXIT_FAILURE)
 			RET("Init_line() failed.\n", EXIT_FAILURE);
 		str[has_label + 1] = ' ';
-		//?
 		(*i)++;
 		if ((int)len != has_label + 1
 			&& !is_only_whitespace(&(str[has_label + 2])))
-		{
-			fprintf(stderr, "ADD INST POST LABEL: |%s| len=%d has_label=%d - only whitespace = %d\n", &(str[has_label + 2]), (int)len, has_label, is_only_whitespace(&(str[has_label + 2])));
 			init_line(file, str + has_label + 2, *i, 0);
-		}
 	}
 	else
 	{
-		fprintf(stderr, "no label  READ INIT=%s\n", str);
 		if (init_line(file, str, *i, T_UNKNOWN) == EXIT_FAILURE)
 			RET("Init_line() failed.\n", EXIT_FAILURE);
 	}
 	return (EXIT_SUCCESS);
 }
 
-//OK - 24L
 int			read_file(t_file *file)
 {
 	char	*str;
@@ -118,7 +100,6 @@ int			read_file(t_file *file)
 			free(str);
 			continue ;
 		}
-		fprintf(stderr, "READ s=%s\n", str);
 		if (add_line(file, str, &i, has_label(str)))
 			return (EXIT_FAILURE);
 		free(str);
