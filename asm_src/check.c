24561111d6d698ee886f03a6cbe14233154546bb
fdeage <fdeage@gmail.com>
2015-02-27 11:01:42 +0100

    LAST: fix norme check

--- asm_src/check.c
+++ asm_src/check.c
@@ -6,7 +6,7 @@
 /*   By: fdeage <fdeage@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/18 23:34:21 by fdeage            #+#    #+#             */
-/*   Updated: 2015/02/26 19:30:29 by fdeage           ###   ########.fr       */
+/*   Updated: 2015/02/27 11:01:08 by fdeage           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 #define G	g_op_tab[inst - 1]
 
 extern t_op	g_op_tab[17];
-
-# include<stdio.h>
 
 /*
 ** check_label() makes sure that the initial '%' doesnt block the checking
@@ -33,19 +31,13 @@ int	is_only_whitespace(const char *s)
 {
 	register size_t	i;
 
-	fprintf(stderr, "only whitespace:  ");
 	i = 0;
 	while (s[i])
 	{
-		fprintf(stderr, "%d", s[i]);
 		if (!ft_isspace(s[i]))
-		{
-			fprintf(stderr, "\n");
 			return (0);
-		}
 		++i;
 	}
-	fprintf(stderr, "\n");
 	return (1);
 }
 
@@ -57,30 +49,11 @@ int	has_final_comment(const char *str)
 	while (str[i])
 	{
 		if (str[i] == FINAL_COMMENT_CHAR)
-		{
-			fprintf(stderr, "HAS FINAL COMMENT OK\n"); //virer
 			return (1);
-		}
 		++i;
 	}
 	return (0);
 }
-
-/*
-int	has_final_comment(t_line *line)
-{
-	t_list	*tmp;
-
-	tmp = line->tokens;
-	while (tmp)
-	{
-		if (TOKEN->type == T_F_COMMENT)
-			return (1);
-		tmp = tmp->next;
-	}
-	return (0);
-}
-*/
 
 int	has_right_params(t_line *line)
 {
@@ -89,36 +62,24 @@ int	has_right_params(t_line *line)
 	size_t	nb;
 	int		new_type;
 
-	fprintf(stderr, "hasrightparam... line = %s\n", line->str);
 	if (!(tmp = line->tokens))
 		return (1);
-	fprintf(stderr, "hrp 1 token = %p", tmp);
 	inst = TOKEN->op->opcode;
-	fprintf(stderr, " 2 ");
 	if (G.nb_params != (int)line->nb_params)
 		RET("Wrong parameters number for the inst.\n", 0);
-	fprintf(stderr, " 3 ");
 	if (!(tmp->next))
-		return (0); //no param
-	fprintf(stderr, " 4 ");
+		return (0);
 	tmp = tmp->next;
 	nb = 0;
-	fprintf(stderr, "inst: %s theor: %d, file: %d\n", G.name, G.nb_params, (int)line->nb_params);
 	while (tmp || (int)nb < G.nb_params)
 	{
-		//fprintf(stderr, "TOKEN->type = %d = %d, expected = %d\n", TOKEN->type, TOKEN->type - 8, G.param_types[nb]);
 		new_type = (TOKEN->type == T_A_DLAB ? 2 : TOKEN->type - 8);
 		new_type = (new_type == T_A_INDLAB - 8 ? 4 : new_type);
-		fprintf(stderr, "TOKEN->type = %d = %d, expected = %d\n", TOKEN->type, new_type, G.param_types[nb]);
 		if (new_type & ~G.param_types[nb])
-		{
-			fprintf(stderr, "ERROR with the type of param\n"); //virer
 			return (0);
-		}
 		tmp = tmp->next;
 		++nb;
 	}
-	fprintf(stderr, "ok\n");
 	return (1);
 }
 
@@ -127,17 +88,13 @@ int	check_label(const char *s)
 	register size_t	i;
 	char			*check;
 
-	fprintf(stderr, "check label... str=%s\n", s);
 	check = LABEL_CHARS;
 	i = 0;
 	while (s[i])
 	{
 		if (!ft_strchr(check, s[i]) && !(i == 0 && s[i] == DIRECT_CHAR)
 			&& !((i == 0 || i == 1) && s[i] == LABEL_CHAR))
-		{
-			fprintf(stderr, "WRONG LABEL CHAR i=%d c = %d\n", (int)i, s[i]); //virer
 			return (EXIT_FAILURE);
-		}
 		++i;
 	}
 	return (EXIT_SUCCESS);
